fdbd9551257ec1d497286b2198eed9222a88ebc8
tlcgeyer <tamlincupido2402@gmail.com>
2024-03-18 21:20:24 +0200

    backend

--- model/index.js
+++ model/index.js
@@ -1,14 +1,12 @@
 import { Users } from "./Users.js";
 import { Products } from "./Products.js";
-import {Cart} from "./Cart.js";
 
 
 let users = new Users()
 let products = new Products()
-// let cart = new Cart()
+
 
 export{
     users,
     products,
-    // cart
 }
