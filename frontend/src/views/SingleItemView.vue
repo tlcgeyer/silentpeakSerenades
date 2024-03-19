565e3c53c3a1b82f27cf89b7ac2b030504dd84c1
tlcgeyer <tamlincupido2402@gmail.com>
2024-03-19 10:29:38 +0200

    singleitem

--- frontend/src/views/SingleItemView.vue
+++ frontend/src/views/SingleItemView.vue
@@ -4,19 +4,50 @@
             <li class="breadcrumb">
                 <router-link to="/" style="text-decoration: none; color: grey; font-size: small;" class="breadcrumb-label"><span>Home</span></router-link>
             </li>
-            <li class="breadcrumb">
-                <router-link :to="{name: 'product', params: {id: product.prodID}} "><span>View More</span></router-link>
-            </li>
             <li class="breadcrumb item-active">
                 <span class="breadcrumb-label" style="color: grey; font-size:small;">Instrument</span>
             </li>
         </ul>
+
+        <div class="mt-4" v-if="product">
+            <div class="row">
+                <div class="col-md-6">
+                <img :src="product.prodUrl" alt="prodPic" loading="lazy" class="img img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h2>{{ product.prodName }}</h2>
+                <p class="text-muted">{{ product.category }}</p>
+                <p class="font-weight-bold">Price: R{{ product.prodAmount }},00</p>
+                <p>{{ product.prodDesc }}</p>
+                <router-link to="/instruments">
+                     <button class="btn btn-primary" id="prim">Go Back</button>
+                </router-link>
+               
+            </div>
+        </div>
     </div>
+    <div v-else>
+        <SpinnerComp></SpinnerComp>
+    </div>
+    </div>
+    <br><br>
 </template>
 
 <script>
+import SpinnerComp from '@/components/SpinnerComp.vue';
+
     export default {
-        
+        computed: {
+            product() {
+                return this.$store.state.product
+            }
+        },
+        mounted(){
+            this.$store.dispatch("fetchProduct", this.$route.params)
+        },
+        components: {
+            SpinnerComp
+        }
     }
 </script>
 
