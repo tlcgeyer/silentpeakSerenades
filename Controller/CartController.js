35e4a7e52602d739a5a53feda5dc5d1521c648bd
tlcgeyer <tamlincupido2402@gmail.com>
2024-03-20 18:13:15 +0200

    backend

--- Controller/CartController.js
+++ Controller/CartController.js
@@ -1,5 +1,5 @@
 import {cart} from '../model/index.js'
-import { express } from 'express';
+import  express  from 'express';
 import bodyParser from 'body-parser';
 import { verifyAToken } from '../middleware/AuthenticateUser.js';
 
--- model/Cart.js
+++ model/Cart.js
@@ -1,96 +1,89 @@
 import { connection as db } from "../config/index.js";
 
 class Cart {
-    //fetching products from cart
-    fetchCart(req, res) {
-        try {
-            const qry = `
-        SELECT cartID, prodID, userID, prodDesc, prodURL, quantity, prodAmount 
-        from Cart
-            WHERE userID = ?
-        `;
-            db.query(qry, [req.body.userID], (err, result) => {
-                if (err) {
-        console.error(err);
-        return res.status(500).json({
-          status: res.statusCode,
-          msg: "Failed to retrieve cart items."
-        });
-      }catch(e) {
-             res.json ({
-        status: res.statusCode,
-        result,
-      });
-    });    
-      }
-  }
-
+  //fetching products from cart
+      fetchCart(req, res){
+        const qry = `
+            SELECT cartID, prodID, userID, prodDesc, quantity, prodAmount
+            FROM Courses;
+        `
+        db.query(qry, (err, results)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                results
+            })
+        })
+    }
+ 
+  
     //adding items to the cart
-    addToCart({prodID, userID, prodDesc, prodURL, quantity, prodAmount}) {
-          const qry = `
+    addToCart({ prodID, userID, prodDesc, prodURL, quantity, prodAmount }) {
+      const qry = `
                 INSERT INTO Cart (prodID, userID, prodDesc, prodURL, quantity, prodAmount) 
                 VALUES (? , ?)
 
             `;
         
-          db.query(qry, [prodID, userID, prodDesc, prodURL, quantity, prodAmount], (err) => {
-          if (err) {
-        console.error(err);
-        return res.status(500).json({
+      db.query(qry, [prodID, userID, prodDesc, prodURL, quantity, prodAmount], (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: res.statusCode,
+            msg: "Failed to add item to cart."
+          });
+        }
+        res.json({
           status: res.statusCode,
-          msg: "Failed to add item to cart."
+          msg: "Added To Cart"
         });
-      }
-            res.json({
-                status: res.statusCode,
-                msg: "Added To Cart"
-            });
-        });
+      });
        
     }
 
     //removing stuff from the cart
-    deleteFromCart(req,res) {
-        const qry = `
+    deleteFromCart(req, res) {
+      const qry = `
                 DELETE FROM Cart 
                 WHERE orderID = ?
             `;
-        db.query(qry, [req.params.id], (err) => {
-           if (err) {
-        console.error(err);
-        return res.status(500).json({
+      db.query(qry, [req.params.id], (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: res.statusCode,
+            msg: "Failed to delete item from cart."
+          });
+        }
+        res.json({
           status: res.statusCode,
-          msg: "Failed to delete item from cart."
+          msg: "Item deleted from cart",
         });
-      }
-      res.json ({
-        status: res.statusCode,
-        msg: "Item deleted from cart",
       });
-    });
-}
+    }
     
     //clearing the cart
-       async clearCart(req, res) {
-        const qry = `
+       clearCart(req, res) {
+      const qry = `
         DELETE FROM Cart
         WHERE userID = ?
         `;
-        db.query(qry, [req.body.userID], (err) => {
-             if (err) {
-        console.error(err);
-        return res.status(500).json({
+      db.query(qry, [req.body.userID], (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: res.statusCode,
+            msg: "Failed to clear the cart."
+          });
+        }
+        res.json({
           status: res.statusCode,
-          msg: "Failed to clear the cart."
+          msg: "Cart successfully cleared!",
         });
-      }
-      res.json({
-        status: res.statusCode,
-        msg: "Cart successfully cleared!",
       });
-    });
+    }
   }
-}
+
 
 export {
     Cart
